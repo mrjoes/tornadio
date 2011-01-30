0ec09610b823df63fe06b38b4c461b84d32f635a
Serge S. Koval <serge.koval@gmail.com>
2011-01-30 05:30:44 -0500

    Fixed websocket support when working behind HAProxy.

--- tornadio/persistent.py
+++ tornadio/persistent.py
@@ -10,6 +10,7 @@
 """
 import logging
 
+import tornado
 from tornado.websocket import WebSocketHandler
 
 from tornadio import proto
@@ -28,6 +29,37 @@ class TornadioWebSocketHandler(WebSocketHandler):
 
         super(TornadioWebSocketHandler, self).__init__(handler.application,
                                                        handler.request)
+
+    # HAProxy websocket fix.
+    # Merged from: https://github.com/facebook/tornado/commit/86bd681ff841f272c5205f24cd2a613535ed2e00
+    def _execute(self, transforms, *args, **kwargs):
+        super(TornadioWebSocketHandler, self)._execute(transforms, *args, **kwargs)
+
+        # Next Tornado will have the built-in support for HAProxy
+        if tornado.version_info <= (1,1,0):
+            # Write the initial headers before attempting to read the challenge.
+            # This is necessary when using proxies (such as HAProxy),
+            # need to see the Upgrade headers before passing through the
+            # non-HTTP traffic that follows.
+            self.stream.write(
+                "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
+                "Upgrade: WebSocket\r\n"
+                "Connection: Upgrade\r\n"
+                "Server: TornadoServer/%(version)s\r\n"
+                "Sec-WebSocket-Origin: %(origin)s\r\n"
+                "Sec-WebSocket-Location: ws://%(host)s%(path)s\r\n\r\n" % (dict(
+                        version=tornado.version,
+                        origin=self.request.headers["Origin"],
+                        host=self.request.host,
+                        path=self.request.path)))
+
+    def _write_response(self, challenge):
+        if tornado.version_info <= (1,1,0):
+            self.stream.write("%s" % challenge)
+            self.async_callback(self.open)(*self.open_args, **self.open_kwargs)
+            self._receive_message()
+        else:
+            super(TornadioWebSocketHandler, self)._write_response(challenge)
 
     def open(self, *args, **kwargs):
         self.connection.reset_heartbeat()
