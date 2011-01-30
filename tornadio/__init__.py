1c45779850d1bef2c1130f76931fb5c0e2dc93f0
Serge S. Koval <serge.koval@gmail.com>
2011-01-30 14:17:54 +0200

    Style fixes.

--- tornadio/__init__.py
+++ tornadio/__init__.py
@@ -9,7 +9,7 @@
     :license: Apache, see LICENSE for more details.
 """
 
-version_info = (0,0,2)
+version_info = (0, 0, 2)
 version = '%d.%d.%d' % version_info
 
 from tornadio.conn import SocketConnection
